94b5988d86f211b5fe7fc27a01a4ae6a8d86bc4f
Vladislav <tekuzar@mail,ru>
2025-01-07 18:09:55 +0300

    Init!

---
+++ src/main/java/com/example/Homework29/Controllers/ShoppingController.java
@@ -0,0 +1,34 @@
+package com.example.Homework29.Controllers;
+
+
+import com.example.Homework29.model.Goods;
+import com.example.Homework29.Services.ShoppingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/store/order")
+public class ShoppingController {
+
+    private ShoppingService shopService;
+
+    public ShoppingController (ShoppingService shopService){
+        this.shopService = shopService;
+    }
+
+    @GetMapping(path = "/add")
+    public String addItem (@RequestParam int ID, @RequestParam String name){
+     return shopService.add(ID,name);                                                 // не могу додумать что должно быть в поле
+    }
+
+    @GetMapping(path = "/get")
+    public Goods getItem(@RequestParam int ID){
+     shopService.get();
+     return (Goods) Map.of();
+    }
+
+}
---
+++ src/main/java/com/example/Homework29/OrderService.java
@@ -0,0 +1,10 @@
+package com.example.Homework29;
+
+import java.util.Map;
+
+public interface OrderService {
+
+    String add (Map<Integer,String> order);
+
+    Map<Integer,String> get();
+}
